1c63e9da5aa9c95c81def06005ffb79240f64b90
Davide Cifariello <davide.cifariello@gmail.com>
2020-08-13 15:55:09 +0200

    version

--- src/main/java/it/dav/demo/DemoApplication.java
+++ src/main/java/it/dav/demo/DemoApplication.java
@@ -1,12 +1,15 @@
 package it.dav.demo;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class DemoApplication {
 
 	public static void main(String[] args) {
+		log.info("VERSION {}", 0);
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
